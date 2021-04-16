1458ac255ae41417d7db4073ff6367a988b81f9c
Mohamed Yasser <81998076+ElTawab@users.noreply.github.com>
2021-04-16 21:28:13 +0200

    Update Students_structure.h

--- Students_structure.h
+++ Students_structure.h
@@ -4,7 +4,7 @@ typedef struct
 {
 	char name[30];
     int ID;
-	short int birthdate[3];        //element #0 =day  element #2=month    element #3=year
-	short int score;              //score of last year of the student
+  int birthdate[3];        //element #0 =day  element #2=month    element #3=year
+	int score;              //score of last year of the student
 }student;
 #endif
