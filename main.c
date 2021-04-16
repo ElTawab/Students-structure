462089249f52889f3dfd779f8c6e7bd87130c3d0
Mohamed Yasser <81998076+ElTawab@users.noreply.github.com>
2021-04-16 17:58:09 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,12 +4,5 @@ student stud;
 int main()
 {
 	printf("Welcome\n");
-	stud.birthdate[2]=2004;
-	stud.name[0]='M';
-	stud.name[1]='o';
-	stud.name[2]='h';
-	stud.name[3]=' ';
-	stud.name[4]='T';
-	printf("birthyear: %d\n",stud.birthdate[2]);
-	printf("Name: %s",stud.name);
+
 }
